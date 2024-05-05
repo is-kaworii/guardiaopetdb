fb14497a1d3830a9d248f5b86093e683c97ffe3b
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-05 19:57:45 -0300

    adiciona rotas para user e animal

--- routers/routes.js
+++ routers/routes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
 
+// user routes
+const userRouter = require('./user.router')
+router.use('/', userRouter)
+
+// animal routes
+const animalRouter = require('./animal.router')
+router.use('/', animalRouter)
+
 module.exports = router;
