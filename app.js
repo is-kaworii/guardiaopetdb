41cd11a337da4d894065c1a0c6d5b0f532dce846
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-05 17:37:51 -0300

    create database start up

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 // Variáveis de ambiente
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routers/routes');
+const conn = require('./database/conn');
+const app = express();
 
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routers/routes')
-const app = express()
+// Database conection
+conn();
 
 // implementando dependencias
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // routes
-app.use('/api', routes)
+app.use('/api', routes);
 
 // start up server
 app.listen(process.env.PORT, () => {
     console.log(`Servidor http inciado em http://localhost:${process.env.PORT}`)
-})
+});
